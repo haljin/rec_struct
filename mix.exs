e3bec3b0dc45111e8bf52daeb3ec69d808bf0d52
haljin <paul.shinobi@gmail.com>
2017-09-13 11:21:41 +0200

    Hex prep

--- mix.exs
+++ mix.exs
@@ -4,10 +4,12 @@ defmodule RecStruct.Mixfile do
   def project do
     [
       app: :rec_struct,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.2.0",
+      elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description()
     ]
   end
 
@@ -22,6 +24,20 @@ defmodule RecStruct.Mixfile do
   defp deps do
     [
       {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+  
+  defp description do
+    "Library for defining record structures, that is Elixir structs that automatically map to Erlang records."
+  end
+  
+  defp package do
+    [
+      name: "rec_struct",
+      files: ["lib/*", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Pawel Antemijczuk"],
+      licenses: ["MIT License"],
+      links: %{"GitHub" => "https://github.com/haljin/rec_struct"}
     ]
   end
 end
